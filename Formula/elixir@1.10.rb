9e926a06fc864a20374b8a5eabbc151233851b99
Aleksey <hukendo@yandex.ru>
2020-04-11 22:17:45 +0000

    Update elixir@1.10.rb

--- Formula/elixir@1.10.rb
+++ Formula/elixir@1.10.rb
@@ -5,13 +5,6 @@ class ElixirAT110 < Formula
   sha256 "bf10dc5cb084382384d69cc26b4f670a3eb0a97a6491182f4dcf540457f06c07"
   head "https://github.com/elixir-lang/elixir.git"
 
-  bottle do
-    cellar :any_skip_relocation
-    sha256 "086572927b0ae7cecfbad39ed9231db201b32a9229f17f928e2bda96d17d5419" => :catalina
-    sha256 "3b8caa1aaa5e09b4a4e031e70b63e008a212c51df5e9d76b9bb1cce4eeca101d" => :mojave
-    sha256 "2425dfe5c15c51794a92b744e5373bfb64f32874524441e91bc67420e53348f5" => :high_sierra
-  end
-
   depends_on "erlang"
 
   def install
